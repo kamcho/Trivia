31de441b670037b3e7e2323a003f051274b8816a
kamcho <njokevin9@gmail.com>
2025-09-30 13:21:39 -0400

    perf: reduce white flash; prep Tailwind build scaffolding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./home/templates/**/*.html",
-    "./**/templates/**/*.html"
+    "./**/templates/**/*.html",
+    "./**/*.py"
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          DEFAULT: '#4a6cf7',
-          dark: '#3453e6'
-        },
-        surface: '#111827'
-      }
-    }
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
 
 
